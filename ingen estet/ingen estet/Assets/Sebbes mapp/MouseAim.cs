65a294507d24c28d577313ce442c1dc88a84eeda
Zeblet <sebastian.l.f@hotmail.com>
2019-03-01 10:16:52 +0100

    MOUSE AIM

---
+++ ingen estet/ingen estet/Assets/Sebbes mapp/MouseAim.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseAim : MonoBehaviour {
+
+    public Vector2 direction;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        mouseAim();
+
+        
+	}
+
+    void mouseAim()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        direction = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
+
+        transform.right = direction;
+    }
+}
